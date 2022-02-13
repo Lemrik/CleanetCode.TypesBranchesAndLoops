33bdccb9ed26fc9955c60f5f4b2494262089e8f8
Lemrik-home <Lemrik33work@gmail.com>
2022-02-14 00:02:24 +0300

    first commit

---
+++ CleanetCode.TypesBranchesAndLoops/CleanetCode.GuessingNumberGame/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello There!");
+
+Console.WriteLine("Как вас зовут");
+string? userName = Console.ReadLine();
+
+Console.WriteLine($"Привет {userName}." +
+    $" Я загадал для тебя число от 0 до 99! Попробуй отгдать");
+
+
+
+
+int secretNumber = 42;
+int userNumber = -1;
+bool isIntNumber = false;
+
+do
+{
+    Console.WriteLine("Введи число от 0 до 99");
+    string? userInput = Console.ReadLine();
+    isIntNumber = int.TryParse(userInput, out userNumber);
+
+    if (true)
+    {
+
+    }
+
+} while (!isIntNumber) ;
+
+
+if (userNumber > secretNumber)
+{
+    Console.WriteLine($"Ваше число {userNumber} больше, чем загаданное");
+}
+else if (userNumber < secretNumber)
+{
+    Console.WriteLine($"Ваше число {userNumber} меньше, чем загаданное");
+
+}
+else
+{
+    Console.WriteLine("Вы победили!");
+}
---
+++ CleanetCode.TypesBranchesAndLoops/CleanetCode.TypesBranchesAndLoops/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// литералы - значения
+
+// numeric types
+Console.WriteLine(1);
+Console.WriteLine(42);
+Console.WriteLine(637658);
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+
+Console.WriteLine(10L);
+Console.WriteLine(42L);
+Console.WriteLine(425210L);
+Console.WriteLine(long.MaxValue);
+Console.WriteLine(long.MinValue);
+
+Console.WriteLine(42.0f);
+
+// char
+Console.WriteLine('L');
+// string
+Console.WriteLine("Lemrik");
+
+Console.WriteLine(DateTime.Now);
+Console.WriteLine(Guid.NewGuid());
+
+// переменные - variables
+
+string name = "Dimon";
+Console.WriteLine(name);
+name = "Lemrik";
+Console.WriteLine(name);
+
+//декримент
+int countOfVideos = 100;
+countOfVideos = countOfVideos + 1;
+countOfVideos = ++countOfVideos;
+countOfVideos += 1;
+Console.WriteLine(countOfVideos);
+
+Console.WriteLine(10 + 4);
+Console.WriteLine(10 - 4);
+Console.WriteLine(10 * 4);
+Console.WriteLine(12 / 4);
+Console.WriteLine(22 % 10);
+
+Console.WriteLine(true || false);
+Console.WriteLine(false || false);
+Console.WriteLine(true && false);
+Console.WriteLine(true && true);
+
+Console.WriteLine();
+
+Console.WriteLine(10 > 20);
+Console.WriteLine(10 == 20);
+Console.WriteLine(10 <= 20);
+Console.WriteLine(10 != 20);
+
+Console.WriteLine(10 > 20 || 19 < 35);
+
+
+
+
+
+
+
+
+
+
+
